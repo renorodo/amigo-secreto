b5636ee51c54e8f08831e4772ee3d1308852a22f
Rodolfo Rodriguez <rodolforodriguezlozano@gmail.com>
2025-02-03 11:38:12 -0600

    Add a line to get the friends and saving on the variable amigos

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo(){
+
+    amigos.push(document.getElementById('amigo').value);
+    
+}
